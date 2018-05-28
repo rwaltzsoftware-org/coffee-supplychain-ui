663792608e96e311060b553a1fbfe94c4333876f
Nitish Kasar <nitish@imperialsoftech.com>
2018-05-28 17:14:46 +0530

    - Added Base Structure

--- js/app/app.js
+++ js/app/app.js
@@ -1,10 +1,23 @@
-	var SupplyChainStorageAddress = "0xed19118faf41505aa39f68d8db05311970ebface";
+var globIcoAddress = {
+	'CoffeeMain': "0xed19118faf41505aa39f68d8db05311970ebface",
+};
 
-	function initContract()
-	{
-		contract = new web3.eth.Contract(SupplyChainStorageAbi,SupplyChainStorageAddress);	
+var globMainContract = false;
 
-		console.log(contract);
-
-		$(window).trigger("contractReady");
+window.addEventListener('load', function() 
+{  
+  	if (typeof web3 !== 'undefined') {
+	  web3 = new Web3(web3.currentProvider);
+	} else {
+	  // set the provider you want from Web3.providers
+	  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 	}
+
+	initContract();
+});
+
+function initContract()
+{
+	globMainContract = new web3.eth.Contract(CoffeeSupplyChainAbi,globIcoAddress.CoffeeMain);	
+	$(window).trigger("mainContractReady");
+}
