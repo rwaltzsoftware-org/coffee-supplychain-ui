1c9b6a7cb2ec664ea8c7826164821297a2020b2e
Nitish Kasar <nitish@imperialsoftech.com>
2018-05-29 11:10:43 +0530

    - Added handler for events

--- js/app/admin.js
+++ js/app/admin.js
@@ -7,8 +7,31 @@
 
 /* Nitish Code Starts */
 
+(function()
+{
+    $(window).on("mainContractReady",function(){
 
+        
+    });
+});
 
+function addCultivationBatch()
+{
+
+}
+
+function getAllEvents(contractRef) {
+    contractRef.getPastEvents('Transfer', {
+        fromBlock: 0,
+        filter: {
+            to: coinbase
+        }
+    }).then(function (events) {
+        console.log(events);
+
+        $("#transactions tbody").html(buildTransactionData(events));
+    });
+}
 /* Nitish Code Ends */
 
 
